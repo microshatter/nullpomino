28813b2fb580e15fa1aedf19b9248ecde523a5f8
microshatter <2976466417@qq.com>
2026-09-23 20:48:05 +0800

    Added Tower Climb based on TETR.IO Quick Play 2(imcomplete)

---
+++ nullpomino-core/src/main/java/mu/nu/nullpo/game/subsystem/mode/TowerClimb.java
@@ -0,0 +1,85 @@
+package mu.nu.nullpo.game.subsystem.mode;
+
+import java.util.Random;
+
+import mu.nu.nullpo.game.component.BGMStatus;
+import mu.nu.nullpo.game.component.Controller;
+import mu.nu.nullpo.game.event.EventReceiver;
+import mu.nu.nullpo.game.play.GameEngine;
+import mu.nu.nullpo.util.CustomProperties;
+import mu.nu.nullpo.util.GeneralUtil;
+
+import org.apache.log4j.Logger;
+
+public class TowerClimb extends AbstractMode {
+    static final Logger log = Logger.getLogger(TowerClimb.class);
+    
+    private static final int CURRENT_VERSION = 1;
+
+    private static final float[] tableFloors = {0, 50, 150, 300, 450, 650, 850, 1100, 1350, 1650};
+
+    private float altitude;
+
+    private int kos;
+
+    private int bgmlv;
+    
+    @Override
+    public String getName() {
+        return "TOWER CLIMB";
+    }
+
+    @Override 
+    public void playerInit(GameEngine engine, int playerID) {
+        // Base Engine loader
+        owner = engine.owner;
+        receiver = engine.owner.receiver;
+
+        if (owner.replayMode) {
+            loadSetting(owner.replayProp);
+            
+        }
+    }
+
+    protected void loadSetting(CustomProperties prop) {}
+
+    protected void saveSetting(CustomProperties prop) {}
+
+    private void setStartBgmlv(GameEngine engine) {
+        bgmlv = 0;
+    }
+
+    private void setSpeed(GameEngine engine) {}
+
+    @Override
+    public void renderSetting(GameEngine engine, int playerID) {}
+    
+    @Override 
+    public boolean onReady(GameEngine engine, int playerID) {
+        return false
+    }
+
+    @Override 
+    public void startGame(GameEngine engine, int playerID) {}
+    
+    @Override 
+    public void renderLast(GameEngine engine, int playerID) {}
+
+
+    @Override 
+    public boolean onMove(GameEngine engine, int playerID) {
+        return super.onMove(engine, playerID);
+    }
+
+    @Override 
+    public void onLast(GameEngine engine, int playerID) {}
+
+    @Override 
+    public boolean onGameOver(GameEngine engine, int playerID) {}
+
+    @Override 
+    public void renderResult(GameEngine engine, int playerID) {}
+
+    @Override 
+    public void saveReplay(GameEngine engine, int playerID, CustomProperties prop) {}
+}
